9c04994123f136555379393c128f7a37b6d037a0
Geoffrey Wagner <wageof@gmail.com>
2021-07-26 22:32:36 -0500

    Get BTRFS working

---
+++ configure.ps1
@@ -0,0 +1,11 @@
+# This rsync is because docker on windows is dumb especially with WSL
+wsl rsync -aPv --delete ./ /root/arch-automation
+
+# Ensure we have the newest container
+wsl docker build -t ansible:latest /root/arch-automation/
+
+# Run the full test automation
+wsl docker run -it --rm `
+    -v ~/arch-automation/:/ansible `
+    -w /ansible `
+    ansible:latest ansible-playbook -vvvv -i inventory.yaml -l test.valewood.lab -e create_image=false --vault-password-file /vault-password-file arch_configure.yaml
